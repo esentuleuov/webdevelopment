bda00d79b906bbbe74ed0357689dfc4cfd36a6cf
esentuleuov <47265989+esentuleuov@users.noreply.github.com>
2021-03-03 22:34:56 +0600

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
+import { ProductsComponent } from "./products/products.component";
+import { ProductsDetailComponent } from "./products-detail/products-detail.component";
+import { CategoriesComponent } from "./categories/categories.component";
+import { CategoriesDetailComponent } from "./categories-detail/categories-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/categories', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductsDetailComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'categories/:id', component: CategoriesDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
