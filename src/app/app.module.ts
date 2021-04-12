f9f46044b1a7ea1908751ac6c84d41684065cbd2
esentuleuov <47265989+esentuleuov@users.noreply.github.com>
2021-04-12 15:38:50 +0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }    from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductsDetailComponent } from './products-detail/products-detail.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { CategoriesDetailComponent } from './categories-detail/categories-detail.component';
-import { AppRoutingModule } from './app-routing.module';
-
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    ProductsDetailComponent,
-    CategoriesComponent,
-    CategoriesDetailComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+         { path: '', component: ProductListComponent },
+       ])
+   ],
+   declarations: [
+      AppComponent,
+      ProductAlertsComponent,
+      TopBarComponent,
+      ProductListComponent,
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
